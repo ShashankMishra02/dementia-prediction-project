bea3b9941e519ac31f6550fb827f81fe42cbbb30
shashank <sm2450918@gmail.com>
2023-05-09 11:21:17 +0530

    form created

--- project.py
+++ project.py
@@ -1,11 +1,34 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request,redirect
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/',methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    if request.method == 'POST':
+      print('submitted')
+      user_name = request.form.get('user_Name')
+      visit_date = request.form.get('Visit')
+      mr_delay = request.form.get('MR Delay')
+      mf = request.form.get('Male/Female')
+      hand = request.form.get('Right/Left')
+      age = request.form.get('Age')
+      educ = request.form.get('educ') 
+      ses = request.form.get('SES')
+      mmse = request.form.get('MMSE') 
+      cdr = request.form.get('CDR')
+      etiv = request.form.get('eTIV')
+      nwbv = request.form.get('nWBV')
+      asf = request.form.get('ASF')
+      data = [user_name,visit_date,mr_delay,mf,
+              hand,age,educ,ses,mmse,cdr,etiv,nwbv,asf]
+      print(data)
+      redirect('/result')
+    return render_template('form (1).html')
+
+@app.route('/result', methods=['GET', 'POST'])
+def result():
+    return render_template('result.html')
+
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=8000, debug=True)
- 
